c911a451256a755298a14968b0b996ef79e956d6
Martin Schaaf <703355+mschaaf@users.noreply.github.com>
2020-07-16 10:38:34 +0200

    iniial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.SneakyThrows;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+@SneakyThrows
+	@Test
+	void contextLoads() {
+          System.out.println("bla");
+          System.out.println("bla");
+          System.out.println("bla");
+          System.out.println("bla");
+          System.out.println("bla");
+	}
+
+}
